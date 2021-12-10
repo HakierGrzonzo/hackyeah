2d6f90196f3173c4fca6b430120180a41b537e93
Leszczyński Mateusz <achillesv23@gmail.com>
2021-12-11 00:45:06 +0100

    TempFile Update

--- tempfileTest.py
+++ tempfileTest.py
@@ -1,8 +1,20 @@
 import tempfile
+import os
+from os.path import join
 
-with tempfile.TemporaryFile() as fp:
-    with open ("a/01_hackaton_copy.txt","r") as file:
-        print(file.read().replace('\n',''))
-        fp.write(bytes(file.read().replace('\n',''),"UTF8"))
+import subprocess, os, platform
+
+
+with tempfile.NamedTemporaryFile(dir="tmp/",mode="w+b",delete=False,prefix="TEST",suffix=".pdf") as fp:
+    with open ("a/03_hackaton_task.pdf","r") as file:
+        x=file.read().replace('\n','')
+        print(x)
+        fp.write(bytes(x,"UTF8"))
         fp.seek(0)
         print(fp.read())
+    if platform.system() == 'Darwin':      
+        subprocess.call(('open', fp.name))
+    elif platform.system() == 'Windows':  
+        os.startfile(fp.name)
+    else:                                 
+        subprocess.call(('xdg-open', fp.name)) 
