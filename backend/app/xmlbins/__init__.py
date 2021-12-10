816e5824cff28da5b5402aab1b01902397bdbcdd
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-12-10 20:56:48 +0100

    refactor to return file

--- backend/app/xmlbins/__init__.py
+++ backend/app/xmlbins/__init__.py
@@ -1,13 +1,17 @@
 from bs4 import BeautifulSoup as Soup
 import base64
+import tempfile
 
 def get_bytes_from_xml(file_name: str):
     """
-    Returns filename and bytes of a signed file from xml
+    Returns file created from base64 encoded data in XML
     """
     soup = Soup(open(file_name, "rb").read(), features="lxml")
-    return base64.b64decode(soup.find("str:zalacznik")['nazwapliku']).decode(), \
-        base64.b64decode(soup.find("str:danezalacznika").string)
+    file = tempfile.NamedTemporaryFile(suffix=base64.b64decode(soup.find("str:zalacznik")['nazwapliku']).decode())
+    file.write(base64.b64decode(soup.find("str:danezalacznika").string))
+    file.flush()
+    file.seek(0)
+    return file
 
 if __name__ == "__main__":
     print(get_bytes_from_xml("/home/hakiergrzonzo/Downloads/test_2.txt"))
