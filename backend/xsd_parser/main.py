1a91dd813ad4e4de8a1f189e1591b90523ebf18b
Kamil Kowalski <kamilkowalski314@gmail.com>
2021-12-11 00:25:59 +0100

    Working fetcher, this time i swear

--- backend/xsd_parser/main.py
+++ backend/xsd_parser/main.py
@@ -6,25 +6,29 @@ from urllib.parse import urljoin
 
 listOfXsdTypes = ['application/octet-stream','text/xml']
 def downloadXSD(baseUrl,linkUrl):
+    if len(linkUrl) == 0:
+        return []
     if linkUrl[0]=='.' or linkUrl[0] == '/':
         linkUrl = urljoin(baseUrl,linkUrl)
         
-    print(linkUrl)
-    r = requests.get(linkUrl)
+    try:
+        r = requests.get(linkUrl)
+    except:
+        print("failed to connect to : ",baseUrl,linkUrl)
+        return []
 
     if r.status_code == 200 or r.status_code == 202:
-        if r.headers['content-type'] in listOfXsdTypes:
+        #print(r.headers['content-type'].split(";")[0])
+        if r.headers['content-type'].split(";")[0] in listOfXsdTypes:
+            print(linkUrl)
             return r.text
         else:
-            print("we need to go deeper")
-            soup = BeautifulSoup(r.text)#,features='lxlm')
+            soup = BeautifulSoup(r.text,features='lxml')
             results = []
             for link in soup.findAll('a'):
-                try:
+                if link.get('href') is not None and link.get('href')[-3:] in ['xsd','xml']:
                     results.append(downloadXSD(linkUrl,link.get('href')))
-                except:
-                    print("failed to download schema", linkUrl ," : ",link.get('href'))
-        return results;
+        return results
 
 def downloadSchemas(filepath):
 
@@ -34,10 +38,13 @@ def downloadSchemas(filepath):
     for name in  namespaces:
         r = requests.get(namespaces[name])
         if r.status_code == 200 or r.status_code == 202:
+            #try:
             downloadXSD("",namespaces[name])
+            #except Exception as e:
+                #print("Failed to download schema")
         else:
             print ("Error downloading schema: ",namespaces[name])
 
-    return schemas;
+    return schemas
 
 downloadSchemas("xml/file1")
