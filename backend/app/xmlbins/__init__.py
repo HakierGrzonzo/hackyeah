ec8419f51747ceafcd076b3c2b9218015063b061
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-12-10 20:47:52 +0100

    get filename from xml

--- backend/app/xmlbins/__init__.py
+++ backend/app/xmlbins/__init__.py
@@ -2,8 +2,12 @@ from bs4 import BeautifulSoup as Soup
 import base64
 
 def get_bytes_from_xml(file_name: str):
+    """
+    Returns filename and bytes of a signed file from xml
+    """
     soup = Soup(open(file_name, "rb").read(), features="lxml")
-    return base64.b64decode(soup.find("str:danezalacznika").string)
+    return base64.b64decode(soup.find("str:zalacznik")['nazwapliku']).decode(), \
+        base64.b64decode(soup.find("str:danezalacznika").string)
 
 if __name__ == "__main__":
     print(get_bytes_from_xml("/home/hakiergrzonzo/Downloads/test_2.txt"))
