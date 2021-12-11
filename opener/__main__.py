2b681b29b7329bb78f1baa116e5e289b255cebf1
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-12-11 01:11:25 +0100

    Add file rename

--- opener/__main__.py
+++ opener/__main__.py
@@ -4,6 +4,7 @@ import magic
 import mimetypes
 import tempfile
 import os
+import shutil
 from zipfile import ZipFile
 
 
@@ -11,8 +12,16 @@ if __name__ == "__main__":
     file = sys.argv[1]
     file_mime = magic.from_file(file, mime=True)
     with tempfile.TemporaryDirectory() as temp_dir:
+        new_file_path = os.path.join(temp_dir, f"{hash(file)}." + file.split(".")[-1])
+        shutil.copy(file, new_file_path)
+        file = new_file_path
         while True:
-            if "text/xml" == file_mime:
+            if not file.endswith(ext := mimetypes.guess_extension(file_mime)):
+                # Jeśli nie zgadza się rozszerzenie, zmień je
+                new_file_path = os.path.join(temp_dir, f"{hash(file)}" + ext)
+                os.rename(file, new_file_path)
+                file = new_file_path
+            elif "text/xml" == file_mime:
                 try:
                     with open(file, "r") as f:
                         if 'xmlns:wnio="http://epuap.gov.pl/fe-model-web/wzor_lokalny/EPUAP-----/podpisanyPlik/"' in f.readlines()[0]:
@@ -22,7 +31,6 @@ if __name__ == "__main__":
                             finalFile = get_bytes_from_xml(f.read(), temp_dir)
                             file = finalFile.name
                             file_mime = magic.from_file(file, mime=True)
-                            print(file_mime, mimetypes.guess_extension(file_mime))
                         else:
                             # mamy jakiś poprawny, aczkolwiek nie podpisany xml
                             break
