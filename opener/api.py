7c38f9367fb56ab7712703f8bd8739baf0ffaa05
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-12-11 15:43:06 +0100

    cors

--- opener/api.py
+++ opener/api.py
@@ -1,6 +1,22 @@
 from .types import OpenFileResponse
 from .open_file import process_file
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
