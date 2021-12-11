1534ba467b5f7f61be45a45cd8a483341548991c
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-12-11 12:16:56 +0100

    pdf redaction

--- opener/__main__.py
+++ opener/__main__.py
@@ -1,4 +1,5 @@
 from .xmlbins import get_bytes_from_xml
+from .pdf_redact import redact_strings_from_pdf
 import sys
 import magic
 import mimetypes
@@ -7,18 +8,26 @@ import os
 import shutil
 from zipfile import ZipFile
 
+def get_new_file_path(root: str, source: str, extension = None):
+    if extension is None:
+        extension = source.split(".")[-1]
+    if not extension.startswith("."):
+        extension = "." + extension
+    return os.path.join(root, str(abs(hash(source))) + extension)
+
+
 
 if __name__ == "__main__":
     file = sys.argv[1]
     file_mime = magic.from_file(file, mime=True)
     with tempfile.TemporaryDirectory() as temp_dir:
-        new_file_path = os.path.join(temp_dir, f"{hash(file)}." + file.split(".")[-1])
+        new_file_path = get_new_file_path(temp_dir, file)
         shutil.copy(file, new_file_path)
         file = new_file_path
         while True:
             if not file.endswith(ext := mimetypes.guess_extension(file_mime)):
                 # Jeśli nie zgadza się rozszerzenie, zmień je
-                new_file_path = os.path.join(temp_dir, f"{hash(file)}" + ext)
+                new_file_path = get_new_file_path(temp_dir, file, ext)
                 os.rename(file, new_file_path)
                 file = new_file_path
             elif "text/xml" == file_mime:
@@ -45,7 +54,15 @@ if __name__ == "__main__":
                 for file in zf.namelist():
                     file = os.path.join(temp_dir, file)
                     file_mime = magic.from_file(file, mime=True)
+            elif file_mime == "application/pdf":
+                # redact stuff
+                new_file_path = get_new_file_path(temp_dir, file)
+                redact_strings_from_pdf(file, ["z?weryfik"], new_file_path)
+                file = new_file_path
+                break
             else:
                 break
         print(file, file_mime)
+        #os.startfile(file)
+        input()
         
