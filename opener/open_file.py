24c8291a3bd891cadf54211ac651193479fb492b
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-12-11 16:13:05 +0100

    fix fastapi 2

--- opener/open_file.py
+++ opener/open_file.py
@@ -30,79 +30,81 @@ def open_file_in_default(file_path: str) -> None:
 
 def process_file(file: str):
     file_mime = magic.from_file(file, mime=True)
-    with tempfile.TemporaryDirectory() as temp_dir:
-        new_file_path = get_new_file_path(temp_dir, file)
-        shutil.copy(file, new_file_path)
-        file = new_file_path
-        while True:
-            print(file_mime)
-            if not file.endswith(ext if (ext := mimetypes.guess_extension(file_mime)) is not None else ""):
-                # Jeśli nie zgadza się rozszerzenie, zmień je
-                new_file_path = get_new_file_path(temp_dir, file, ext)
-                os.rename(file, new_file_path)
-                file = new_file_path
-            elif file_mime == "text/plain" and open(file, "r").read().startswith("<"):
-                # Jeżeli coś się zaczyna jakimś tagiem to pewnie jest to jakaś forma XML
+    #with tempfile.TemporaryDirectory() as temp_dir:
+    temp_directory = tempfile.TemporaryDirectory()
+    temp_dir = temp_directory.name
+    new_file_path = get_new_file_path(temp_dir, file)
+    shutil.copy(file, new_file_path)
+    file = new_file_path
+    while True:
+        print(file_mime)
+        if not file.endswith(ext if (ext := mimetypes.guess_extension(file_mime)) is not None else ""):
+            # Jeśli nie zgadza się rozszerzenie, zmień je
+            new_file_path = get_new_file_path(temp_dir, file, ext)
+            os.rename(file, new_file_path)
+            file = new_file_path
+        elif file_mime == "text/plain" and open(file, "r").read().startswith("<"):
+            # Jeżeli coś się zaczyna jakimś tagiem to pewnie jest to jakaś forma XML
+            file = prependFile(file)
+            file_mime = magic.from_file(file, mime=True)
+            res = OpenFileResponse(mime = file_mime)
+            res.path_to_xml = file
+            return res
+        elif "text/xml" == file_mime:
+            try:
+                with open(file, "r") as f:
+                    if 'xmlns:wnio="http://epuap.gov.pl/fe-model-web/wzor_lokalny/EPUAP-----/podpisanyPlik/"' in f.read():
+                        # mamy plik podpisany
+                        f.seek(0)
+                        # TODO(hakiergrzonzo): sprawdzanie podpisu
+                        finalFile = get_bytes_from_xml(f.read(), temp_dir)
+                        file = finalFile.name
+                        file_mime = magic.from_file(file, mime=True)
+                    else:
+                        # mamy jakiś poprawny, aczkolwiek nie podpisany xml
+                        file = prependFile(file)
+                        res = OpenFileResponse(mime = file_mime)
+                        res.path_to_xml = file
+                        return res
+            except UnicodeDecodeError:
+                # mamy xml ale nie utf8
                 file = prependFile(file)
-                file_mime = magic.from_file(file, mime=True)
                 res = OpenFileResponse(mime = file_mime)
                 res.path_to_xml = file
                 return res
-            elif "text/xml" == file_mime:
-                try:
-                    with open(file, "r") as f:
-                        if 'xmlns:wnio="http://epuap.gov.pl/fe-model-web/wzor_lokalny/EPUAP-----/podpisanyPlik/"' in f.read():
-                            # mamy plik podpisany
-                            f.seek(0)
-                            # TODO(hakiergrzonzo): sprawdzanie podpisu
-                            finalFile = get_bytes_from_xml(f.read(), temp_dir)
-                            file = finalFile.name
-                            file_mime = magic.from_file(file, mime=True)
-                        else:
-                            # mamy jakiś poprawny, aczkolwiek nie podpisany xml
-                            file = prependFile(file)
-                            res = OpenFileResponse(mime = file_mime)
-                            res.path_to_xml = file
-                            return res
-                except UnicodeDecodeError:
-                    # mamy xml ale nie utf8
-                    file = prependFile(file)
-                    res = OpenFileResponse(mime = file_mime)
-                    res.path_to_xml = file
-                    return res
-                        
-            elif file_mime in ["application/zip"]:
-                # decompress zip
-                zf = ZipFile(file, "r")
-                zf.extractall(temp_dir)
-                for file in zf.namelist():
-                    file = os.path.join(temp_dir, file)
-                    file_mime = magic.from_file(file, mime=True)
-            elif file_mime in ["application/x-tar"]:
-                # decompress tar
-                zf = TarFile(file, "r")
-                zf.extractall(temp_dir)
-                for file in zf.getnames():
-                    file = os.path.join(temp_dir, file)
-                    file_mime = magic.from_file(file, mime=True)
-            elif file_mime in ["application/gzip"]:
-                # decompress gnuzip
-                new_file_path = get_new_file_path(temp_dir, file)
-                with gzip.open(file) as gz, open(new_file_path, "wb+") as f:
-                    f.write(gz.read())
-                file = new_file_path
+                    
+        elif file_mime in ["application/zip"]:
+            # decompress zip
+            zf = ZipFile(file, "r")
+            zf.extractall(temp_dir)
+            for file in zf.namelist():
+                file = os.path.join(temp_dir, file)
                 file_mime = magic.from_file(file, mime=True)
-            elif file_mime in ["application/x-7z-compressed"]:
-                # decompress 7z
-                with SevenZipFile(file, mode="r") as z:
-                    z.extractall(path=temp_dir)
-                    for file in z.getnames():
-                        file = os.path.join(temp_dir, file)
-                        file_mime = magic.from_file(file, mime=True)
-            else:
-                break
-        res = OpenFileResponse(mime = file_mime)
-        res.path_to_file = file
-        open_file_in_default(file)
-        return res
+        elif file_mime in ["application/x-tar"]:
+            # decompress tar
+            zf = TarFile(file, "r")
+            zf.extractall(temp_dir)
+            for file in zf.getnames():
+                file = os.path.join(temp_dir, file)
+                file_mime = magic.from_file(file, mime=True)
+        elif file_mime in ["application/gzip"]:
+            # decompress gnuzip
+            new_file_path = get_new_file_path(temp_dir, file)
+            with gzip.open(file) as gz, open(new_file_path, "wb+") as f:
+                f.write(gz.read())
+            file = new_file_path
+            file_mime = magic.from_file(file, mime=True)
+        elif file_mime in ["application/x-7z-compressed"]:
+            # decompress 7z
+            with SevenZipFile(file, mode="r") as z:
+                z.extractall(path=temp_dir)
+                for file in z.getnames():
+                    file = os.path.join(temp_dir, file)
+                    file_mime = magic.from_file(file, mime=True)
+        else:
+            break
+    res = OpenFileResponse(mime = file_mime)
+    res.path_to_file = file
+    open_file_in_default(file)
+    return res
         
